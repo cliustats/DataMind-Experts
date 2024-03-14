547a6d46ddf8dffff6cf832f2734d1a4fceb9529
Chang <chang.liu@wustl.edu>
2024-03-13 22:15:13 -0500

    Create Workers With The Highest Salaries.py

---
+++ stratascratch/Workers With The Highest Salaries.py
@@ -0,0 +1,11 @@
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+# worker.head()
+
+title_worker_id = title.rename(columns={'worker_ref_id': 'worker_id'})
+merged_df = pd.merge(worker, title_worker_id, on='worker_id')
+max_salary = merged_df[merged_df['salary'] == merged_df['salary'].max()]
+result = max_salary[['worker_title']].rename(columns={"worker_title": "best_paid_title"})
+result
