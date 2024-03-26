903d62b3750e0f9dce78fa46f59e54371fbd91a5
Chang <chang.liu@wustl.edu>
2024-03-26 09:53:41 -0500

    SQL

--- stratascratch/Workers_With_The_Highest_Salaries.py
+++ stratascratch/Workers_With_The_Highest_Salaries.py
@@ -9,3 +9,6 @@ merged_df = pd.merge(worker, title_worker_id, on='worker_id')
 max_salary = merged_df[merged_df['salary'] == merged_df['salary'].max()]
 result = max_salary[['worker_title']].rename(columns={"worker_title": "best_paid_title"})
 result
+
+
+# SQL solution
