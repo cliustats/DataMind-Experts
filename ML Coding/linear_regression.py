0c1e8b5c0a27315d80739c2d87ca0233bda70eb7
Chang <chang.liu@wustl.edu>
2024-06-07 10:49:26 -0500

    Update linear_regression.py

--- ML Coding/linear_regression.py
+++ ML Coding/linear_regression.py
@@ -64,4 +64,39 @@ class LinearRegression:
         X = np.hstack((np.ones((n, 1)), X))
         return X @ self.W
 
-class
+
+# test
+X = np.array([2,2], [4,5], [7,8])
+y = np.arrary([7. 17, 26])
+
+# fit linear regression model
+lr = LinearRegression()
+lr.fit(X, y)
+print(lr.W)
+
+# make predictions on new data
+X_new = np.array([10, 11], [12, 13])
+y_pred = lr.predict(X_new)
+print(y_pred)
+
+
+
+#######   improvements on simple LR   ########
+
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, regul=0):
+        self.regul = regul
+        self.W = None
+
+    def fit(self, X, y, lr=0.01, num_iter=1000):
+        # input validation
+        if len(X) != len(y) or len(X) == 0:
+            raise ValueError("X and y must have the same length and cannot be empty")
+
+        # add bias term to X -> [1, X]
+        X = np.hstack((np.ones(len(X), 1), X))
+
+        # Initialize W to zeros
+        self.W = np.zeros(X.shape[1])
