5ff2a405dc9de7c7aa8295f9aec926aa328aa085
Chang <chang.liu@wustl.edu>
2024-06-11 10:01:41 -0500

    Update decision_tree.py

--- ML Coding/decision_tree.py
+++ ML Coding/decision_tree.py
@@ -8,4 +8,44 @@ def entropy(p):
         return -p * np.log2(p) - (1-p) * np.log2(1-p)
 
 def split_indices(X, index_feature):
-    
+    """Given a dataset and an index feature, return two lists for the
+    two split nodes.
+
+    """
+    left_indices = []
+    right_indices = []
+    for i, x in enumerate(X):
+        if x[index_feature] == 1:
+            left_indices.append(i)
+        else:
+            right_indices.append(i)
+    return left_indices, right_indices
+
+
+def weighted_entropy(X, y, left_indices, right_indices):
+    """
+    This function takes the splitted dataset, the indices we chose
+    to split and return the weight entropy.
+    """
+    w_left = len(left_indices)/len(X)
+    w_right = len(right_indices)/len(X)
+    p_left = sum(y[left_indices])/len(left_indices)
+    p_right = sum(y[right_indices])/len(right_indices)
+
+    weighted_entropy = w_left * entropy(p_left) + w_right * entropy(p_right)
+    return weighted_entropy
+
+
+
+left_indices, right_indices = split_indices(X_train, 0)
+weighted_entropy(X_train, y_train, left_indices, right_indices)
+
+
+def information_gain(X, y, left_indices, right_indices):
+    """
+    Here, X has elements in the node and y is their respective classes
+    """
+    p_node = sum(y)/len(y)
+    h_node = entropy(p_node)
+    w_entropy = weighted_entropy(X, y, left_indices, right_indices)
+    return h_node - w_entropy
