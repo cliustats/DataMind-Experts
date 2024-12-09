f11f6e09b0e2a331c2a298b99d1b28bb000ef014
Chang <chang.liu@wustl.edu>
2024-12-08 22:46:10 -0600

    Update Practice.py

--- ML Coding/Practice.py
+++ ML Coding/Practice.py
@@ -754,3 +754,23 @@ print(titanic_df.head())
 
 # Drop the original 'fare' column
 titanic_df.drop(columns=['fare'], inplace=True)
+
+
+################################################################
+            Outlier Detection
+################################################################
+--- Z-score Method ---
+
+# Calculate Z-scores
+titanic_df['age_zscore'] = np.abs((titanic_df.age - titanic_df.age.mean()) / titanic_df.age.std())
+
+# Get rows of outliers according to the Z-score method (using a threshold of 3)
+outliers_zscore = titanic_df[(titanic_df['age_zscore'] > 3)]
+print(outliers_zscore)
+"""
+     survived  pclass   sex   age  ...  embark_town  alive  alone age_zscore
+630         1       1  male  80.0  ...  Southampton    yes   True   3.462699
+851         0       3  male  74.0  ...  Southampton     no   True   3.049660
+
+[2 rows x 16 columns]
+"""
