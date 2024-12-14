00d5276a308cb92641cdf16c384643d935f44842
Chang <chang.liu@wustl.edu>
2024-12-13 23:04:42 -0600

    Update Python_Tips.py

--- ML Coding/Python_Tips.py
+++ ML Coding/Python_Tips.py
@@ -190,6 +190,58 @@ for item in items:
 print(new_items)
 # Output: ['A', 'C', 'D', 'E']
 
+
+######################
+# Module = a file containing code you want to include in your program
+#          use 'import' to include a module (built-in or your own)
+#          useful to break up a large program reusable seperate files
+
+######################
+# Scope resolution = (LEGB) local -> enclosed -> global -> built-in
+
+######################
+# super() = Function used in a child class to call methods from a parent class (superclass)
+#           Allows you to extend the functionality of the inherited methods
+
+
+class Shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+    def describe(self):
+        print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'}")
+
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+
+    def describe(self):
+        super().describe()
+        print(f"It is a circle with an area of {3.14 * self.radius * self.radius}cm^2")
+
+
+class Square:
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+    def describe(self):
+        print(f"It is a square with an area of {self.width * self.width}cm^2")
+        super().describe()
+
+
+class Triangle:
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+    def describe(self):
+        print(f"It is a triangle with an area of {self.width * self.height / 2}cm^2")
+        super().describe()
 ################################################################
 #                        Decorator
 ################################################################
